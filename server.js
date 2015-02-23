c1da68cb5c2a51b9af3b32006803194d9e469fd1
Daniel Chiu <daniechiu@gmail.com>
2015-02-23 00:11:59 -0500

    Adds canvas calls, which get broadcast to all clients.

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({ port: 8080 });
 
+wss.broadcast = function(data) {
+  wss.clients.forEach(function(client) {
+    client.send(data);
+  });
+}
+
 wss.on('connection', function connection(ws) {
   console.log("Client connected.");
 
   ws.on('message', function(message) {
-    var coordinates = JSON.parse(message);
-    console.log(coordinates);
-    ws.send(JSON.stringify(coordinates));
+    wss.broadcast(message);
   });
 });
