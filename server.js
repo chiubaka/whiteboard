7a2007ebcde89aea1b2bed4434a2467b26b8ff02
Daniel Chiu <daniechiu@gmail.com>
2015-02-22 21:27:37 -0500

    Adds client page and sends messages back and forth between server and client.

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
-  ws.on('draw', function(data) {
-    console.log("Message received");
+  console.log("Client connected.");
+
+  ws.on('message', function(message) {
+    var coordinates = JSON.parse(message);
+    console.log(coordinates);
+    ws.send(JSON.stringify(coordinates));
   });
 });
